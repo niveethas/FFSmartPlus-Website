bc59070b0ae2a2ce50a9b7cd99b94f9a11ebbd17
niveethas <61502706+niveethas@users.noreply.github.com>
2022-11-15 15:32:59 +0000

    Add project files.

---
+++ FFSmartPlus-Website/Pages/ItemList.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ClientAPI;
+using Microsoft.AspNetCore.Components;
+
+
+namespace FFSmartPlus_Website.Pages
+{
+    public partial class ItemList
+    {
+        public ICollection<ItemDto> Items { get; set; }
+        protected async override Task OnInitializedAsync()
+        {
+            string baseurl = "https://localhost:7041";
+            var httpclient = new HttpClient();
+            var _client = new FFSBackEnd(baseurl, httpclient);
+            Items = await _client.ItemAllAsync();
+            //The .ItemAllAsync referes to the different API post/get/put/delete commands (they're named confusingly)
+
+        }
+    }
+}
+
+
+
+
