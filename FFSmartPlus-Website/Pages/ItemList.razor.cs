7c9f44d784484c9a5342e64f5c4dfaa1de9dd475
niveethas <61502706+niveethas@users.noreply.github.com>
2022-12-06 14:51:06 +0000

    Addition of comments, datetime picker, button functionality

--- FFSmartPlus-Website/Pages/ItemList.razor.cs
+++ FFSmartPlus-Website/Pages/ItemList.razor.cs
@@ -3,7 +3,9 @@ using System.Runtime.CompilerServices;
 using ClientAPI;
 using Microsoft.AspNetCore.Components;
 using MatBlazor;
-
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+using System.Text.Encodings.Web;
 
 namespace FFSmartPlus_Website.Pages
 {
@@ -11,27 +13,24 @@ namespace FFSmartPlus_Website.Pages
     {
         public ICollection<UnitsDto> unitStock { get; set; }
         public ICollection<ItemDto> itemsInfo { get; set; }
-
+        public Item newItemResponse { get; set; }
+        
         public ItemDto? itemInfo {get;set;}
         public NewItemDto addItem { get; set; }
-        public ICollection<NewUnitDto> addUnit { get; set; }
+        public NewUnitDto addUnit { get; set; }
+
         public string inputItemName;
-        public string inputItemQuantity;
+        public string inputUnitQuantity;
         public string inputSupplierID;
         public string inputUnitDesc;
         public string inputItemMinStock;
-        public string inputUnitExpDate;
-
-
+        public DateTime inputUnitExpDate;
+        public string itemErrorMessage;
         private string inputID { get; set; }
 
         protected async override Task OnInitializedAsync()
         {
-            string baseurl = "https://localhost:7041";
-            var httpclient = new HttpClient();
-            var _client = new FFSBackEnd(baseurl, httpclient);
-            unitStock = await _client.UnitAsync(long.Parse(inputID));
-            //itemsInfo = await _client.ItemAllAsync();
+           
         }
 
         public async Task loadItem(string itemID)
@@ -39,35 +38,64 @@ namespace FFSmartPlus_Website.Pages
             string baseurl = "https://localhost:7041";
             var httpclient = new HttpClient();
             var _client = new FFSBackEnd(baseurl, httpclient);
+
             itemsInfo = await _client.ItemAllAsync();
-            unitStock = await _client.UnitAsync(long.Parse(inputID));
-            itemInfo = await _client.Item2Async(long.Parse(inputID));
-            Console.WriteLine(itemInfo.Name);
+
+            if (Regex.IsMatch(inputID, "^[0-9]*$")) {
+
+                unitStock = await _client.UnitAsync(long.Parse(inputID));
+                itemInfo = await _client.Item2Async(long.Parse(inputID));
+            }
+            // When the user searches an item, this function is called
+            // All existing items wil be loaded in
+            // The user's input is validated and passed to find the relevant item and unit info
+
         }
         public async Task deleteItem(long id)
         {
             string baseurl = "https://localhost:7041";
             var httpclient = new HttpClient();
             var _client = new FFSBackEnd(baseurl, httpclient);
-            Console.WriteLine(id);
+
+
             await _client.Item4Async(id);
             itemsInfo = await _client.ItemAllAsync();
             itemInfo = null;
+            //function is called when the user presses the delete item button
+            //the is of the item on display is passed in and the items are reloaded
+            //itemInfo is set as null so the page resets
 
         }
 
-        public async Task addNewItem(string name, string unitDesc, string minStock, string supplierID )
+        public async Task addNewItem(string name, string unitDesc, string minStock, string supplierID, string quantity, DateTime expiryDate )
         {
             string baseurl = "https://localhost:7041";
             var httpclient = new HttpClient();
             var _client = new FFSBackEnd(baseurl, httpclient);
 
-            addItem.Name = name;
-            addItem.UnitDesc = unitDesc;
-            addItem.SupplierId = Int32.Parse(supplierID);
-            addItem.MinimumStock = Convert.ToDouble(minStock);
+            var newNID = new NewItemDto();
+            var newAUD = new NewUnitDto();
 
-            await _client.ItemAsync(addItem);
+
+
+            newNID.Name = name;
+            newNID.UnitDesc = unitDesc;
+            newNID.SupplierId = Int32.Parse(supplierID);
+            newNID.MinimumStock = Convert.ToDouble(minStock);
+
+            newAUD.ExpiryDate = expiryDate;
+            newAUD.Quantity = Convert.ToDouble(quantity);
+
+            newItemResponse = await _client.ItemAsync(newNID);
+            Console.WriteLine(newItemResponse.Id);
+            itemsInfo = await _client.ItemAllAsync();
+            await _client.AddAsync(newItemResponse.Id,newAUD);
+            
+            //add new unit
+            //add comments
+            //clean up code - get rid of baseurl connections and rename variables
+            //search by name?
+            
 
         }
 
