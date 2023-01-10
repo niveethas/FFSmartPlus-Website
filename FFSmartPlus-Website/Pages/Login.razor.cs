46174413a1a5e26a2c48fcc728d469e766bca9be
niveethas <61502706+niveethas@users.noreply.github.com>
2023-01-10 21:51:58 +0000

    Sign In Textboxes

---
+++ FFSmartPlus-Website/Pages/Login.razor.cs
@@ -0,0 +1,31 @@
+﻿using ClientAPI;
+using Microsoft.AspNetCore.Components;
+using System.Runtime.CompilerServices;
+
+namespace FFSmartPlus_Website.Pages
+{
+    public partial class Login
+    {
+        [Inject]
+        public FFSBackEnd? _client { get; set; }
+
+        public string inputUsername;
+        public string inputPassword;    
+
+
+        protected async override Task OnInitializedAsync()
+        {
+           
+
+        }
+
+        public async Task userLogin(string username, string password)
+        {
+            var newLogin = new LoginModel();
+            newLogin.Username = username;
+            newLogin.Password = password;
+            await _client.LoginAsync(newLogin);
+        }
+
+    }
+}
