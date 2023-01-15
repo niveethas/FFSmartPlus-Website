5340f627154a4acdb65d9e9130b53b28c5760aa9
niveethas <61502706+niveethas@users.noreply.github.com>
2023-01-15 12:46:13 +0000

    Basic Layout and functionality completed

--- FFSmartPlus-Website/Pages/ManageUsers.razor.cs
+++ FFSmartPlus-Website/Pages/ManageUsers.razor.cs
@@ -1,5 +1,7 @@
 ﻿using ClientAPI;
+using MatBlazor;
 using Microsoft.AspNetCore.Components;
+
 
 namespace FFSmartPlus_Website.Pages
 {
@@ -9,25 +11,60 @@ namespace FFSmartPlus_Website.Pages
         [Inject]
         public FFSBackEnd? _client { get; set; }
 
+        public ICollection<String> allUsers;
+
         public string inputUsername;
         public string inputPassword;
         public string inputEmail;
+        public bool additionSuccess;
+        public bool successDelete;
+        [Inject]
+        protected IMatToaster Toaster { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            allUsers = await _client.AllAsync();
+        }
 
         public async Task addUser(string username, string password, string email)
         {
+            //create new instance of model that has registration information
+            //assign input information to model
             var newRM = new RegisterModel();
             newRM.Username = username;
             newRM.Password = password;
             newRM.Email = email;
             await _client.RegisterAsync(newRM);
+            await checkSuccess();
+            allUsers = await _client.AllAsync();
+            
         }
 
-        public async Task deleteUser()
+        //bring back all users to delete the ones in a drop down list style, or search by username
+
+        public async Task deleteUser(string username)
         {
-            //nick will make back end code
-            //then complete this
-            //make pop up when the code is complete (provide success or failure message)
+            //delete user via username; it is unique
+            successDelete =  await _client.DeleteUserAsync(username);
+            allUsers = await _client.AllAsync();
+            
         }
+
+        private async Task checkSuccess()
+        {
+            var currentCount = allUsers.Count;
+            allUsers = await _client.AllAsync();
+           if (currentCount < allUsers.Count)
+            {
+                 additionSuccess = true;
+            }
+            else
+            {
+                additionSuccess = false;
+            }
+        }
+
+        
 
     }
 }
--- FFSmartPlus-Website/Program.cs
+++ FFSmartPlus-Website/Program.cs
@@ -11,4 +11,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMatBlazor();
 builder.Services.AddScoped<FFSBackEnd>(sp => new FFSBackEnd(builder.Configuration["APIBase"], new HttpClient()));
+builder.Services.AddMatToaster(config =>
+{
+    config.Position = MatToastPosition.TopRight;
+    config.PreventDuplicates = true;
+    config.NewestOnTop = true;
+    config.ShowCloseButton = true;
+    config.MaximumOpacity = 95;
+    config.VisibleStateDuration = 3500;
+});
 await builder.Build().RunAsync();
