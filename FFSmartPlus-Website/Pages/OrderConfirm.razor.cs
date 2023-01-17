2f7c1831c53b06f87e5e43498e7a501cb72179d1
niveethas <61502706+niveethas@users.noreply.github.com>
2023-01-17 18:56:22 +0000

    Order Confirmation Pages set up

---
+++ FFSmartPlus-Website/Pages/OrderConfirm.razor.cs
@@ -0,0 +1,22 @@
+﻿using ClientAPI;
+using MatBlazor;
+using Microsoft.AspNetCore.Components;
+
+namespace FFSmartPlus_Website.Pages
+{
+    public partial class OrderConfirm
+    {
+        [Inject]
+        public FFSBackEnd? _client { get; set; }
+        [Inject]
+        protected IMatToaster Toaster { get; set; }
+
+        public ICollection<SupplierOrderDto> orderItems;
+
+        protected async override Task OnInitializedAsync()
+        {
+            orderItems = await _client.GenerateOrderAsync();
+        }
+
+    }
+}
