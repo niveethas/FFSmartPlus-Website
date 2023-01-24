3ae61cd48350e1c22b0f6ff4f177b4b89a7487d9
niveethas <61502706+niveethas@users.noreply.github.com>
2023-01-24 13:26:56 +0000

    Account Management Functioanlity Complete

--- FFSmartPlus-Website/CurrentUserRoles.cs
+++ FFSmartPlus-Website/CurrentUserRoles.cs
@@ -5,8 +5,8 @@ namespace FFSmartPlus_Website
     public class CurrentUserRoles
 {
         public static List<string>? _role;
-
+        public static string? _user;
         public List<string>? Role { get { return _role; } set { _role = value; } }
-
+        public string? User { get { return _user; }set { _user = value; } }
     }
 }
