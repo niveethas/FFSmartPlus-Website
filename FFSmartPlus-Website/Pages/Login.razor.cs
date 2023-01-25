6a7d73d97b96019381afa3f02937e80a56842b74
niveethas <61502706+niveethas@users.noreply.github.com>
2023-01-25 16:16:37 +0000

    Added JWT Authorisation

--- FFSmartPlus-Website/Pages/Login.razor.cs
+++ FFSmartPlus-Website/Pages/Login.razor.cs
@@ -19,6 +19,7 @@ namespace FFSmartPlus_Website.Pages
         public FFSBackEnd? _client { get; set; }
         [Inject]
         protected IMatToaster Toaster { get; set; }
+       
 
         public string inputUsername;
         public string inputPassword;
@@ -42,6 +43,7 @@ namespace FFSmartPlus_Website.Pages
                 loginResponse = await _client.LoginAsync(newLogin);
 
                 var token = new JwtSecurityToken(jwtEncodedString: loginResponse.Token.ToString());
+                _client.Authorisation(loginResponse.Token.ToString());
                 var temp = token.Claims.ToList();
                 CurrentUserRoles currentRole = new CurrentUserRoles();
                 List<string> roles = new List<string>();
