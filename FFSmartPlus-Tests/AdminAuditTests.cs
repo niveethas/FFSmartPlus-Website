0d2de4734daa53876118bd2070c2193574c37198
JRad99 <jaradjbailey@gmail.com>
2023-01-29 18:40:32 +0000

    Added Admin Audit Tests - requires Fix

---
+++ FFSmartPlus-Tests/AdminAuditTests.cs
@@ -0,0 +1,95 @@
+﻿using ClientAPI;
+using FFSmartPlus_Website.Pages;
+using FFSmartPlus_Website;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFSmartPlus_Tests
+{
+    [TestClass]
+    public class AdminAuditTests
+    {
+        AdminAudit _adminAudit;
+
+        List<UnitListDto> expStockList;
+
+        public AdminAuditTests()
+        {
+            _adminAudit = new AdminAudit();
+            _adminAudit._client = new FFSBackEnd("https://localhost:7041/", new HttpClient());
+
+            var newLogin = new LoginModel();
+            newLogin.Username = "Admin2";
+            newLogin.Password = "@Admin123"; // Password requires a symbol, capital letter and 3 numbers
+            _adminAudit._client.LoginAsync(newLogin);
+            var loginCode = _adminAudit._client.LoginAsync(newLogin);
+            _adminAudit._client.Authorisation(loginCode.Result.Token);
+        }
+
+        //expStockToList()
+        [TestMethod]
+        public async Task T1_expStockToList()
+        {
+            var newAUD = new NewUnitDto();
+            newAUD.ExpiryDate = DateTime.Now.AddDays(-7);
+            newAUD.Quantity = 5;
+
+            long validId = 1;
+
+            await _adminAudit._client.AddAsync(validId, newAUD);
+
+            _adminAudit.allExpStock = await _adminAudit._client.ExpiryAsync();
+
+            expStockList = _adminAudit.expStockToList();
+
+            Assert.IsTrue(expStockList.Count() > 0);
+        }
+
+        [TestMethod]
+        public async Task T2_deleteExpStock()
+        {
+            await _adminAudit.deleteExpStock();
+
+            Assert.AreEqual(TestConsts.TRUE_STR, _adminAudit.deletionSuccess);
+        }
+
+        [TestMethod]
+        public async Task T3_expStockToList_NoneExpired()
+        {
+            _adminAudit.allExpStock = await _adminAudit._client.ExpiryAsync();
+            List<UnitListDto> newList = _adminAudit.expStockToList();
+
+            CollectionAssert.AreNotEqual(expStockList, newList);
+        }
+
+        [TestMethod]
+        public async Task T4_deleteExpStock_NoneExpired()
+        {
+            await _adminAudit.deleteExpStock();
+
+            Assert.AreEqual(TestConsts.TRUE_STR, _adminAudit.deletionSuccess);
+        }
+
+        [TestMethod]
+        public async Task T5_getAuditHistory_Valid()
+        {
+            string days = "3";
+            await _adminAudit.getAuditHistory(days);
+
+            Assert.AreEqual(TestConsts.TRUE_STR, _adminAudit.auditSuccess);
+        }
+
+        [TestMethod]
+        public async Task T6_getAuditHistory_Invalid_StrCha()
+        {
+            string daysInv = "ABC";
+            await _adminAudit.getAuditHistory(daysInv);
+
+            Assert.AreEqual(TestConsts.FALSE_STR, _adminAudit.auditSuccess);
+        }
+
+    }
+}
--- FFSmartPlus-Tests/SignUpTests.cs
+++ FFSmartPlus-Tests/SignUpTests.cs
@@ -35,7 +35,7 @@ namespace FFSmartPlus_Tests
         public async Task TA1_SignUp_ValidUser()
         {
             await signUp._client.DeleteUserAsync(validUsername);
-            await signUp.userSignUp(validUsername, validPassword, validEmail);
+            await signUp.userSignUp(validUsername, validPassword, validEmail, validPassword);
 
             Assert.AreEqual(TestConsts.TRUE_STR, signUp.signUpSuccess);
         }
@@ -43,7 +43,7 @@ namespace FFSmartPlus_Tests
         [TestMethod]
         public async Task TA2_SignUp_InvalidUser_ExistingUser()
         {
-            await signUp.userSignUp(validUsername, validPassword, validEmail);
+            await signUp.userSignUp(validUsername, validPassword, validEmail, validPassword);
             Assert.AreEqual(TestConsts.FALSE_STR, signUp.signUpSuccess);
         }
 
@@ -51,7 +51,7 @@ namespace FFSmartPlus_Tests
         public async Task TA3_SignUp_InvalidPassword_NoNumbers()
         {
             string invalidPassword = "@Password";
-            await signUp.userSignUp(validUsernameToo, invalidPassword, validEmail);
+            await signUp.userSignUp(validUsernameToo, invalidPassword, validEmail, invalidPassword);
             Assert.AreEqual(TestConsts.FALSE_STR, signUp.signUpSuccess);
         }
 
@@ -59,7 +59,7 @@ namespace FFSmartPlus_Tests
         public async Task TA4_SignUp_InvalidPassword_NoSpecial()
         {
             string invalidPassword = "Password123";
-            await signUp.userSignUp(validUsernameToo, invalidPassword, validEmail);
+            await signUp.userSignUp(validUsernameToo, invalidPassword, validEmail, invalidPassword);
             Assert.AreEqual(TestConsts.FALSE_STR, signUp.signUpSuccess);
         }
 
@@ -67,21 +67,21 @@ namespace FFSmartPlus_Tests
         public async Task TA5_SignUp_InvalidPassword_NoCapital()
         {
             string invalidPassword = "@password123";
-            await signUp.userSignUp(validUsernameToo, invalidPassword, validEmail);
+            await signUp.userSignUp(validUsernameToo, invalidPassword, validEmail, invalidPassword);
             Assert.AreEqual(TestConsts.FALSE_STR, signUp.signUpSuccess);
         }
 
         [TestMethod]
         public async Task TA6_SignUp_InvalidPassword_Blank()
         {
-            await signUp.userSignUp(validUsernameToo, blankString, validEmail);
+            await signUp.userSignUp(validUsernameToo, blankString, validEmail, blankString);
             Assert.AreEqual(TestConsts.FALSE_STR, signUp.signUpSuccess);
         }
 
         [TestMethod]
         public async Task TA7_SignUp_InvalidUsername_Blank()
         {
-            await signUp.userSignUp(blankString, validPassword, validEmail);
+            await signUp.userSignUp(blankString, validPassword, validEmail, validPassword);
             Assert.AreEqual(TestConsts.FALSE_STR, signUp.signUpSuccess);
         }
 
@@ -89,16 +89,30 @@ namespace FFSmartPlus_Tests
         public async Task TA8_SignUp_InvalidEmail_InvalidFormat()
         {
             string invalidEmail = "test";
-            await signUp.userSignUp(validUsernameToo, validPassword, invalidEmail);
+            await signUp.userSignUp(validUsernameToo, validPassword, invalidEmail, validPassword);
             Assert.AreEqual(TestConsts.FALSE_STR, signUp.signUpSuccess);
         }
 
         [TestMethod]
         public async Task TA9_SignUp_InvalidEmail_Blank()
         {
-            await signUp.userSignUp(validUsernameToo, validPassword, blankString);
+            await signUp.userSignUp(validUsernameToo, validPassword, blankString, validPassword);
             Assert.AreEqual(TestConsts.FALSE_STR, signUp.signUpSuccess);
         }
 
+
+        [TestMethod]
+        public async Task TA9_SignUp_InvalidPassword_NotMatching()
+        {
+            string validPasswordAlt = "@PasswordToo123";
+            await signUp.userSignUp(validUsernameToo, validPassword, blankString, validPasswordAlt);
+            Assert.AreEqual(TestConsts.FALSE_STR, signUp.signUpSuccess);
+        }
+
+        [TestMethod]
+        public async Task TestCleanup_DeleteNewUser()
+        {
+            await signUp._client.DeleteUserAsync(validUsername);
+        }
     }
 }
