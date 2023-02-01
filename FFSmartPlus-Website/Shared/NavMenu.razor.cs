0d02c9a26800f644034f116eb1e73e3664a382d4
niveethas <61502706+niveethas@users.noreply.github.com>
2023-02-01 14:35:02 +0000

    Fixing page restrictions, nav menu options and sign out

--- FFSmartPlus-Website/Shared/NavMenu.razor.cs
+++ FFSmartPlus-Website/Shared/NavMenu.razor.cs
@@ -6,53 +6,11 @@ using Microsoft.AspNetCore.Mvc.Internal;
 namespace FFSmartPlus_Website.Shared
 {
     public partial class NavMenu
-{
-        public List<string> currentUserRole;
-        public string temp;
-
-        [Inject]
-        public NavigationManager uriHelper { get; set; }
-
-        //protected override async Task OnInitializedAsync()
-        //{
-            
-            
-        //}
-
-        protected async void OnAfterRender(bool firstRender)
-        {
-            Console.WriteLine(GetRoles());
-            StateHasChanged();
-            //if (firstRender)
-            //{
-            //    var timer = new Timer(new TimerCallback(_ =>
-            //    {
-            //        uriHelper.NavigateTo(uriHelper.Uri, forceLoad: true);
-            //    }), null, 20000, 20000);
-            //}
-            //Attempting to get nav menu to regenerate after login
-        }
-
-
-        public bool GetRoles()
+    {
+         public void signOutRole()
         {
             CurrentUserRoles? i = new CurrentUserRoles();
-            currentUserRole = CurrentUserRoles._role;
-            StateHasChanged();
-            if (currentUserRole != null)
-            {
-                temp = currentUserRole.First();
-                if (currentUserRole.Contains("Admin"))
-                {
-                    Console.WriteLine("contains admin");
-                }
-                Console.WriteLine(temp);
-                return true;
-            }
-            else
-            {
-                return false;   
-            }
+            CurrentUserRoles._role = null;
         }
     }
 }
