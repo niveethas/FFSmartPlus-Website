bd2dac1b8a4ac4b76513976eef1c75b911e62d88
JRad99 <jaradjbailey@gmail.com>
2023-02-04 22:11:28 +0000

    removing redundant tests

--- FFSmartPlus-Tests/AdminAuditTests.cs
+++ FFSmartPlus-Tests/AdminAuditTests.cs
@@ -73,29 +73,6 @@ namespace FFSmartPlus_Tests
             Assert.AreEqual(TestConsts.TRUE_STR, _adminAudit.deletionSuccess);
         }
 
-        // --- Audit Tests Removed as cannot run StateHasChanged() in unit tests
-
-        [Ignore]
-        [TestMethod]
-        public async Task TA5_getAuditHistory_Valid()
-        {
-            // get audit history
-            string days = "3";
-            await _adminAudit.getAuditHistory(days);
-
-            Assert.AreEqual(TestConsts.TRUE_STR, _adminAudit.auditSuccess);
-        }
-
-        [Ignore]
-        [TestMethod]
-        public async Task TA6_getAuditHistory_Invalid_StrCha()
-        {
-            // get audit history - invalid days value - non numerical
-            string daysInv = "ABC";
-            await _adminAudit.getAuditHistory(daysInv);
-
-            Assert.AreEqual(TestConsts.FALSE_STR, _adminAudit.auditSuccess);
-        }
 
     }
 }
